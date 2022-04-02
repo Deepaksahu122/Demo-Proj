7bd8331f7a18a1cb9d773d7634756efc43413d2a
Deepak <deepaksahu122@gmail.com>
2022-04-02 11:22:37 +0530

    Changes on file 1

--- javaprogram.java
+++ javaprogram.java
@@ -1,7 +1,9 @@
 class javaprogram
 {
+	
+	
          public static void main(String args[])
            {
-           System.out.print("Hello");
+           System.out.print("Hello  changes");
             }
 }
